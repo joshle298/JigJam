b2cf2f33c47bf36a7f57d5c34704c155e1024ee0
joshua-le <jdl8176@nyu.edu>
2024-04-12 09:16:11 -0400

    feat: connect to live mongodb cluster

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 const dotenv = require("dotenv")
 dotenv.config()
-mongoose.connect(process.env.DSN);
+
+mongoose.connect(process.env.DATABASE_URL);
 
 // layer schema (optional fields scale, author, text, and color)
 const LayerSchema = new mongoose.Schema({
