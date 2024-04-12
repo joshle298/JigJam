4229eae79bd69ce615485f586e0d45bff9a4f4c9
joshua-le <jdl8176@nyu.edu>
2024-04-12 09:51:59 -0400

    fix: change proxy (client)

--- public/config.js
+++ public/config.js
@@ -1,6 +1,6 @@
 const config = {
     // for production
-    apiBaseUrl: 'https://jigjam.live/'
+    apiBaseUrl: 'https://jigjam-production.up.railway.app/'
     
     // for local testing
     // apiBaseUrl: 'http://localhost:3000/'
