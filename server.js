8b3de8ecd99a903c56cf1d3cd1e2d40f9ab64e97
joshua-le <jdl8176@nyu.edu>
2024-04-12 10:18:00 -0400

    fix: test req.body

--- server.js
+++ server.js
@@ -76,6 +76,7 @@ app.get("/api/rooms", async (req, res) => {
 app.post("/api/user/create", async (req, res) => {
     try {
         console.log(`REQ.BODY: ${req.body}`);
+        console.log(`req.body.user sanitized: ${req.body.user}`);
         const user = new User({
             username: sanitize(req.body.user),
             color: sanitize(req.body.color)
